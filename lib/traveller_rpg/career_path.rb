32e769e630a4311b544831648a48a0885b97c9ff
Rick Hull <rick.hull@gmail.com>
2017-12-05 00:03:54 +0000

    implement Draft
    - implement CareerPath.career_class
    - implement CareerPath.run
    - perform career_count for qualify_check in CareerPath#apply
    - implement CareerPath#draft
    - add CareerPath#report

--- lib/traveller_rpg/career_path.rb
+++ lib/traveller_rpg/career_path.rb
@@ -1,9 +1,41 @@
-require 'traveller_rpg/career'
+require 'traveller_rpg/careers'
 
 module TravellerRPG
   class CareerPath
     class Error < RuntimeError; end
     class Ineligible < Error; end
+
+    DRAFT_CAREERS = {
+      1 => ['Navy', nil],
+      2 => ['Army', nil],
+      3 => ['Marines', nil],
+      4 => ['Merchant', :merchant_marine],
+      5 => ['Scout', nil],
+      6 => ['Agent', :law_enforcement],
+    }
+
+    def self.career_class(str)
+      TravellerRPG.const_get(str.split('::').last)
+    end
+
+    def self.run(careers, character: nil)
+      unless character
+        require 'traveller_rpg/generator'
+        character = Generator.character
+      end
+      puts "\n", character.report(desc: :long, stuff: false, credits: false)
+      path = self.new(character)
+      loop {
+        careers = path.eligible(careers)
+        break if careers.empty?
+        choice = TravellerRPG.choose("Choose a career:", *careers)
+        career = self.career_class(choice).new(character)
+        path.run(career)
+        puts "\n", path.report, "\n"
+        break if TravellerRPG.choose("Exit career mode?", :yes, :no) == :yes
+      }
+      path
+    end
 
     attr_reader :char, :careers
 
@@ -29,7 +61,7 @@ module TravellerRPG
 
     def apply(career)
       raise(Ineligible, career.name) unless self.eligible?(career)
-      if career.qualify_check?(@careers.size)
+      if career.qualify_check?(dm: -1 * @careers.size)
         @char.log "Qualified for #{career.name}"
         self.enter(career)
       else
@@ -39,10 +71,26 @@ module TravellerRPG
     end
 
     def fallback
-      choices = self.eligible(%w{Drifter Army Navy Marines})
-      choice = TravellerRPG.choose("Fallback career:", *choices)
-      career = TravellerRPG.career_class(choice).new(@char)
-      self.enter(career)
+      case TravellerRPG.choose("Fallback career:", :drifter, :draft)
+      when :drifter
+        self.enter TravellerRPG::Drifter.new(@char)
+      when :draft
+        self.draft
+      end
+    end
+
+    # return an active career, no qualification, that has completed basic
+    # training
+    def draft
+      roll = TravellerRPG.roll('d6')
+      puts "Draft roll: #{roll}"
+      dc = self.class::DRAFT_CAREERS.fetch(roll)
+      @char.log "Drafted: #{dc.compact.join(', ')}"
+
+      career = CareerPath.career_class(dc.first).new(@char)
+      career.activate(dc.last)
+      self.basic_training(career)
+      career
     end
 
     def eligible(careers)
@@ -55,7 +103,7 @@ module TravellerRPG
         return false if career.active?
         cls = career.class
       when String
-        cls = TravellerRPG.career_class(career)
+        cls = CareerPath.career_class(career)
       end
       return true if cls == TravellerRPG::Drifter
       !@careers.any? { |c| c.class == cls }
@@ -83,6 +131,11 @@ module TravellerRPG
         @char.log "Acquired basic training skill: #{sym} 0"
         @char.skills[sym] = 0
       }
+    end
+
+    def report(char: true, careers: true)
+      [@char.report,
+       @careers.map(&:report).join("\n\n")].join("\n\n")
     end
   end
 end
