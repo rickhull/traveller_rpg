109f6475979bcd68acc387fe7985a68a2522f92e
Rick Hull <rick.hull@gmail.com>
2017-12-05 20:49:43 +0000

    split skill.rb
    - keep the class definitions
    - move skill hierarchy to skills.rb
    - adjust test/ correspondingly

--- test/skill.rb
+++ test/skill.rb
@@ -1,41 +1,6 @@
 require 'traveller_rpg/skill'
 require 'minitest/autorun'
 
-describe "TravellerRPG.skill?" do
-  it "recognizes subskills" do
-    TravellerRPG.skill?('Animals:Handling').must_equal true
-    TravellerRPG.skill?(:animals, :handling).must_equal true
-  end
-
-  it "must accept a single string arg" do
-    ['Admin', 'Animals', 'Animals:Handling'].each { |valid|
-      TravellerRPG.skill?(valid).must_equal true
-    }
-  end
-
-  it "must reject multiple string args" do
-    proc { TravellerRPG.skill?('This', 'That') }.must_raise ArgumentError
-  end
-
-  it "must accept any number of symbol args" do
-    [[:admin], [:animals], [:animals, :handling]].each { |valid|
-      TravellerRPG.skill?(*valid).must_equal true
-    }
-  end
-end
-
-describe "TravellerRPG.skill" do
-  it "does not recognize subskills" do
-    proc { TravellerRPG.skill('Animals:Handling') }.must_raise KeyError
-    proc { TravellerRPG.skill(:animals, :handling) }.must_raise ArgumentError
-  end
-
-  it "accepts a single symbol to find a Skill or ComplexSkill" do
-    TravellerRPG.skill(:admin).must_be_kind_of Skill
-    TravellerRPG.skill(:animals).must_be_kind_of ComplexSkill
-  end
-end
-
 include TravellerRPG
 
 describe Skill do
