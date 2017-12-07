b638c3b5754797858cc40d48d43380c069e13250
Rick Hull <rick.hull@gmail.com>
2017-12-07 01:37:00 +0000

    split up SPECIALIST tests

--- test/careers.rb
+++ test/careers.rb
@@ -80,74 +80,101 @@ ObjectSpace.each_object(Class).select { |klass|
       end
     end
 
-    it "must have valid SPECIALIST" do
-      defined?(klass::SPECIALIST).wont_be_nil
-      spec = klass::SPECIALIST
-      spec.must_be_kind_of Hash
-      if klass == TravellerRPG::ExampleCareer or
-        klass == TravellerRPG::ExampleMilitaryCareer
-        spec.keys.size.must_equal 1
-      else
-        spec.keys.size.must_equal 3
+    describe "SPECIALIST" do
+      before do
+#        defined?(klass::SPECIALIST).wont_be_nil
+        @specs = klass::SPECIALIST
       end
-      spec.each { |asg, cfg|
-        asg.must_be_kind_of String
-        cfg.must_be_kind_of Hash
-        [:skills, :survival, :advancement, :ranks].each { |key|
-          cfg.key?(key).must_equal true
+
+      it "must have valid SPECIALIST" do
+        @specs.must_be_kind_of Hash
+        if klass == TravellerRPG::ExampleCareer or
+          klass == TravellerRPG::ExampleMilitaryCareer
+          @specs.keys.size.must_equal 1
+        else
+          @specs.keys.size.must_equal 3
+        end
+        @specs.each { |asg, spec|
+          asg.must_be_kind_of String
+          spec.must_be_kind_of Hash
+          [:skills, :survival, :advancement, :ranks].each { |key|
+            spec.key?(key).must_equal true
+          }
         }
+      end
 
-        cfg[:skills].must_be_kind_of Array
-        cfg[:skills].size.must_equal 6
-        cfg[:skills].flatten.each { |skill|
-          if skill.is_a?(Symbol)
-            TravellerRPG::Character::Stats.members.must_include(skill)
-          else
-            skill.must_be_kind_of String
-            puts skill unless TravellerRPG.known_skill? skill
-            TravellerRPG.known_skill?(skill).must_equal true
-          end
-        }
-
-        cfg[:survival].must_be_kind_of Array
-        cfg[:survival].size.must_equal 2
-        stat, check = *cfg[:survival]
-        stat.must_be_kind_of Symbol
-        TravellerRPG::Character::Stats.members.must_include(stat)
-        check.must_be_kind_of Integer
-        (2..12).must_include check
-
-        cfg[:advancement].must_be_kind_of Array
-        cfg[:advancement].size.must_equal 2
-        stat, check = *cfg[:advancement]
-        stat.must_be_kind_of Symbol
-        TravellerRPG::Character::Stats.members.must_include(stat)
-        check.must_be_kind_of Integer
-        (2..12).must_include check
-
-        cfg[:ranks].must_be_kind_of Hash
-        cfg[:ranks].each { |rank, ary|
-          (0..6).must_include rank
-          ary.size.must_be :<=, 3
-          title, skill, _level = *ary
-          if title.nil?
-            skill.wont_be_nil
-          else
-            title.must_be_kind_of String
-          end
-
-          if skill.nil?
-            title.wont_be_nil
-          else
-            if skill.is_a? String
-              TravellerRPG.known_skill?(skill).must_equal true
-            else
-              skill.must_be_kind_of Symbol
+      it "must have valid SPECIALIST :skills" do
+        @specs.values.each { |spec|
+          skills = spec.fetch(:skills)
+          skills.must_be_kind_of Array
+          skills.size.must_equal 6
+          skills.flatten.each { |skill|
+            if skill.is_a?(Symbol)
               TravellerRPG::Character::Stats.members.must_include(skill)
+            else
+              skill.must_be_kind_of String
+              puts skill unless TravellerRPG.known_skill? skill
+              TravellerRPG.known_skill?(skill).must_equal true
             end
-          end
+          }
         }
-      }
+      end
+
+      it "must have valid SPECIALIST :survival" do
+        @specs.values.each { |spec|
+          survival = spec.fetch(:survival)
+          survival.must_be_kind_of Array
+          survival.size.must_equal 2
+          stat, check = *survival
+          stat.must_be_kind_of Symbol
+          TravellerRPG::Character::Stats.members.must_include(stat)
+          check.must_be_kind_of Integer
+          (2..12).must_include check
+        }
+      end
+
+      it "must have valid SPECIALIST :advancement" do
+        @specs.values.each { |spec|
+          advancement = spec.fetch(:advancement)
+          advancement.must_be_kind_of Array
+          advancement.size.must_equal 2
+          stat, check = *advancement
+          stat.must_be_kind_of Symbol
+          TravellerRPG::Character::Stats.members.must_include(stat)
+          check.must_be_kind_of Integer
+          (2..12).must_include check
+        }
+      end
+
+      it "must have valid SPECIALIST :ranks" do
+        @specs.values.each { |spec|
+          ranks = spec.fetch(:ranks)
+          ranks.must_be_kind_of Hash
+          ranks.each { |rank, ary|
+            (0..6).must_include rank
+            ary.size.must_be :<=, 3
+            title, skill, _level = *ary
+
+            # rank_benefit must include a title, skill, or both
+            if title.nil?
+              skill.wont_be_nil
+            else
+              title.must_be_kind_of String
+            end
+
+            if skill.nil?
+              title.wont_be_nil
+            else
+              if skill.is_a? String
+                TravellerRPG.known_skill?(skill).must_equal true
+              else
+                skill.must_be_kind_of Symbol
+                TravellerRPG::Character::Stats.members.must_include(skill)
+              end
+            end
+          }
+        }
+      end
     end
   end
 }
