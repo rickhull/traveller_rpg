342e8a20f63b8fb7c19e35d8c595dffb19b2f6d1
Rick Hull <rick.hull@gmail.com>
2017-12-08 02:25:05 +0000

    rework CareerPath; add tests

--- lib/traveller_rpg/career_path.rb
+++ lib/traveller_rpg/career_path.rb
@@ -5,15 +5,16 @@ require 'traveller_rpg/skill'
 
 module TravellerRPG
   class CareerPath
-    class Error < RuntimeError; end
-    class Ineligible < Error; end
+    class Ineligible < RuntimeError; end
 
     DRAFT_CAREERS = {
-      1 => ['Navy', nil],
-      2 => ['Army', nil],
-      3 => ['Marines', nil],
-      4 => ['Merchant', 'Merchant Marine'],
-      5 => ['Scout', nil],
+      1 => ['Navy'],
+      2 => ['Army'],
+      3 => ['Marines'],
+      # TODO: define Merchant career
+      # 4 => ['Merchant', 'Merchant Marine'],
+      4 => ['Marines'],
+      5 => ['Scout'],
       6 => ['Agent', 'Law Enforcement'],
     }
 
@@ -21,14 +22,17 @@ module TravellerRPG
       TravellerRPG.const_get(str.split('::').last)
     end
 
+    def self.draft_career?(str)
+      DRAFT_CAREERS.values.map(&:first).include? str
+    end
+
     def self.run(careers, character:)
       puts "\n", character.report(desc: :long, stuff: false, credits: false)
       path = self.new(character)
       loop {
-        careers = path.eligible(careers)
+        careers = careers.select { |c| path.eligible? c }
         break if careers.empty?
-        choice = TravellerRPG.choose("Choose a career:", *careers)
-        career = self.career_class(choice).new(character)
+        career = TravellerRPG.choose("Choose a career:", *careers)
         path.run(career)
         puts "\n", path.report, "\n"
         break if TravellerRPG.choose("Exit career mode?", :yes, :no) == :yes
@@ -46,87 +50,71 @@ module TravellerRPG
       @careers = []
     end
 
+    def career(career_name)
+      self.class.career_class(career_name).new(@char)
+    end
+
+    def finished
+      @careers
+    end
+    alias_method :completed, :finished
+
+    def eligible?(career)
+      case career
+      when Career
+        return false if career.status != :new or career.term != 0
+        cls = career.class
+      when String
+        cls = CareerPath.career_class(career)
+      end
+      cls == TravellerRPG::Drifter or !@careers.any? { |c| cls === c }
+    end
+
     # Run career to completion; keep running terms while possible
-    def run(career)
-      career = self.apply(career)
-      loop {
-        career.run_term
-        break unless career.active?
-        break if career.must_exit?
-        next if career.must_remain?
-        break if TravellerRPG.choose("Muster out?", :yes, :no) == :yes
-      }
-      career.muster_out
-      @careers << career
-      career
+    def run(career, asg = nil)
+      run! self.apply career, asg
     end
 
     # Pass eligibility and qualify_check
     # Take Drifter or Draft if disqualified
     # Then enter career
-    def apply(career)
+    def apply(career, asg = nil)
+      career = self.career(career) if career.is_a? String
       raise(Ineligible, career.name) unless self.eligible?(career)
       if career.qualify_check?(dm: -1 * @careers.size)
         @char.log "Qualified for #{career.name}"
-        self.enter(career)
+        self.enter(career, asg)
       else
         @char.log "Did not qualify for #{career.name}"
-        case TravellerRPG.choose("Fallback career:", :drifter, :draft)
-        when :drifter
-          self.enter TravellerRPG::Drifter.new(@char)
-        when :draft
-          self.draft
+        case TravellerRPG.choose("Choose fallback:", 'Drifter', 'Draft')
+        when 'Drifter' then self.enter TravellerRPG::Drifter.new(@char), asg
+        when 'Draft'   then self.draft
         end
       end
     end
 
-    # return an active career, no qualification, that has completed basic
+    def enter(career, asg = nil)
+      raise(Ineligible, career.name) unless self.eligible?(career)
+      @char.log "Entering new career: #{career.name}"
+      enter! career, asg
+    end
+
+    # return an active career, no qualification check, that has completed basic
     # training
     def draft
-      roll = TravellerRPG.roll('d6')
-      puts "Draft roll: #{roll}"
-      dc = self.class::DRAFT_CAREERS.fetch(roll)
-      @char.log "Drafted: #{dc.compact.join(', ')}"
-
-      career = CareerPath.career_class(dc.first).new(@char)
-      career.activate(dc.last)
-      self.basic_training(career)
-      career
-    end
-
-    def eligible(careers)
-      careers.select { |c| self.eligible?(c) }
-    end
-
-    def eligible?(career)
-      case career
-      when Career
-        return false if career.active?
-        cls = career.class
-      when String
-        cls = CareerPath.career_class(career)
-      end
-      return true if cls == TravellerRPG::Drifter
-      !@careers.any? { |c| c.class == cls }
-    end
-
-    def enter(career)
-      raise(Ineligible, career.name) unless self.eligible?(career)
-      raise(Error, "career is already active") if career.active?
-      raise(Error, "career has already started") unless career.term == 0
-      @char.log "Entering new career: #{career.name}"
-      career.activate
-      self.basic_training(career)
-      career
+      roll = TravellerRPG.roll('d6', label: "Draft")
+      career, asg = self.class::DRAFT_CAREERS.fetch(roll)
+      @char.log "Drafted: #{[career, asg].compact.join(', ')}"
+      enter! self.career(career), asg
     end
 
     def basic_training(career)
-      return unless career.term.zero?
+      return career unless career.term.zero?
       if @careers.length > 0
         skills = career.class::SERVICE_SKILLS.flatten.reject { |s|
           @char.skills[s]
         }
-        return if skills.empty?
+        return career if skills.empty?
         skills = [TravellerRPG.choose("Choose service skill:", *skills)]
       else
         # Take "all" SERVICE_SKILLS, but choose any choices
@@ -136,9 +124,9 @@ module TravellerRPG
             skill = skill.reject { |s| @char.skills[s] }
             case skill.size
             when 0
-              return
+              return career
             when 1
-              skills << skill
+              skills << skill.first
             else
               skills << TravellerRPG.choose("Choose service skill:", *skill)
             end
@@ -151,11 +139,34 @@ module TravellerRPG
         @char.skills.provide(skill)
         @char.log "Acquired basic training skill: #{skill}"
       }
+      career
     end
 
     def report(char: true, careers: true)
-      [@char.report,
-       @careers.map(&:report).join("\n\n")].join("\n\n")
+      [@char.report, @careers.map(&:report).join("\n\n")].join("\n\n")
+    end
+
+    private
+
+    # activate a new career, with basic training
+    def enter!(career, asg = nil)
+      self.basic_training(career.activate(asg))
+    end
+
+    # take an active career and run_term until complete
+    # muster_out and update career history
+    def run!(career)
+      raise(Error, "#{career.name} isn't active") unless career.active?
+      loop {
+        career.run_term
+        break unless career.active?
+        break if career.must_exit?
+        next if career.must_remain?
+        break if TravellerRPG.choose("Muster out?", :yes, :no) == :yes
+      }
+      career.muster_out
+      @careers << career
+      career
     end
   end
 end
---
+++ test/career_path.rb
@@ -0,0 +1,195 @@
+require 'traveller_rpg/career_path'
+require 'traveller_rpg/generator'
+require 'minitest/autorun'
+
+include TravellerRPG
+
+describe CareerPath do
+  describe CareerPath.method(:career_class) do
+    it "takes a string and returns a class" do
+      CareerPath.career_class('Drifter').must_equal Drifter
+    end
+  end
+
+  describe CareerPath.method(:draft_career?) do
+    it "takes a career name and recognizes draft destinations" do
+      CareerPath.draft_career?('Army').must_equal true
+      CareerPath.draft_career?('Navy').must_equal true
+      CareerPath.draft_career?('Drifter').must_equal false
+      # these work for now, subject to change
+      CareerPath.draft_career?('ExampleCareer').must_equal false
+      CareerPath.draft_career?('abolutely anything!').must_equal false
+    end
+  end
+
+  describe CareerPath.method(:run) do
+    before do
+      capture_io { @char = Generator.character }
+    end
+
+    it "accepts an Array of career names and returns a CareerPath" do
+      capture_io do
+        CareerPath.run(%w{Drifter Agent},
+                       character: @char).must_be_kind_of CareerPath
+      end
+    end
+
+    it "runs careers until it quits" do
+      path = nil
+      capture_io { path = CareerPath.run(['Drifter'], character: @char) }
+      path.careers.wont_be_empty
+      path.careers.any? { |c| c.is_a?(Drifter) }.must_equal true
+    end
+  end
+
+  describe "new instance" do
+    before do
+      capture_io do
+        @char = Generator.character
+        @path = CareerPath.new(@char)
+      end
+      @career = Scout.new(@char)   # anything but Drifter
+    end
+
+    it "has attrs" do
+      @path.char.must_equal @char
+      @path.careers.must_be_kind_of Array
+      @path.careers.must_be_empty
+    end
+
+    describe "CareerPath#eligible?" do
+      it "accepts a String or a Career" do
+        @path.eligible?('Drifter').must_equal true
+        @path.eligible?(@career).must_equal true
+      end
+
+      it "rejects non-new careers" do
+        capture_io do
+          @path.eligible?(@career.activate).must_equal false
+          @path.eligible?(@career.run_term).must_equal false
+          @path.eligible?(@career.muster_out).must_equal false
+        end
+      end
+
+      it "rejects careers which have been completed" do
+        capture_io { @path.send "run!", @career.activate }
+        @path.eligible?(@career.name).must_equal false
+      end
+    end
+
+    describe "CareerPath#run" do
+      it "returns a Career" do
+        capture_io do
+          @path.run('Drifter').must_be_kind_of Drifter
+          @path.run(@career).must_be_kind_of Career # maybe Drifter or Draft
+        end
+      end
+
+      it "affects Character and career history" do
+        @path.careers.must_be_empty
+        @char.cash_rolls.must_equal 0
+        capture_io { @path.run 'Drifter' }
+        @path.careers.wont_be_empty
+        @path.careers.size.must_equal 1
+        @char.cash_rolls.must_be :>, 0
+        capture_io { @path.run @career }
+        @path.careers.size.must_equal 2
+      end
+    end
+
+    describe "CareerPath#enter" do
+      it "accepts a Career and returns a Career" do
+        capture_io { @path.enter(@career).must_be_kind_of Career }
+      end
+
+      it "raises on a career name (string)" do
+        proc { @path.enter 'Drifter' }.must_raise StandardError
+      end
+
+      it "activates a career and performs basic training" do
+        @career.active?.must_equal false
+        num_skills = @char.skills.count
+        capture_io { @path.enter @career }
+        @career.active?.must_equal true
+        @char.skills.count.must_be :>, num_skills
+      end
+
+      it "rejects ineligible careers" do
+        career = @path.career(@career.name)  # duplicate
+        capture_io { @path.send "run!", career.activate }
+        proc { @path.enter career }.must_raise CareerPath::Ineligible
+        # can't enter Scout a 2nd time, even if it's a new career
+        proc { @path.enter @career }.must_raise CareerPath::Ineligible
+      end
+    end
+
+    describe "CareerPath#apply" do
+      it "accepts a String or a Career and returns an entered Career" do
+        career = nil
+        capture_io { career = @path.apply(@career) }
+        career.must_be_kind_of Career # maybe Drifter or Draft
+        career.active?.must_equal true
+        case career
+        when @career.class
+          # TODO: this can fail if CareerPath.draft_career?(@career.name)
+          # career.must_equal @career
+        when Drifter
+          # ok
+        else
+          CareerPath.draft_career?(career.name).must_equal true
+        end
+      end
+    end
+
+    describe "CareerPath#draft" do
+      it "returns an entered Career" do
+        skill_count = @char.skills.count(subskills: true)
+        career = nil
+        capture_io { career = @path.draft }
+        CareerPath.draft_career?(career.name).must_equal true
+        career.active?.must_equal true
+        @char.skills.count(subskills: true).must_be :>, skill_count + 3
+      end
+    end
+
+    describe "CareerPath#basic_training" do
+      it "accepts a Career, provides Character Skills, returns that Career" do
+        skill_count = @char.skills.count(subskills: true)
+        capture_io do
+          @path.basic_training(@career).must_equal @career
+        end
+        @char.skills.count(subskills: true).must_be :>, skill_count + 3
+      end
+
+      it "provides up to 6 service skills for the first career" do
+        capture_io { @path.basic_training(@career) }
+        ss = @career.class::SERVICE_SKILLS.flatten
+        if ss.size == 6
+          # all present
+          ss.each { |s| @char.skills[s].wont_be_nil }
+        elsif ss.size > 6
+          ss.size.must_be :<=, 9
+          # at least 6 of these are known
+          ss.select { |s| @char.skills[s] }.size.must_be :>=, 6
+        else
+          raise "unexpected SERVICE_SKILLS: #{ss.inspect}"
+        end
+      end
+
+      it "allows only a single service skill for subsequent careers" do
+        career = nil
+        capture_io { career = @path.run @career }
+        skill_count = @char.skills.count
+        capture_io { @path.basic_training @path.career 'Drifter' }
+        case career
+        when Drifter
+          Drifter::SERVICE_SKILLS.flatten.size.must_equal 6 # sanity check
+          # can't learn any new skills the second go-round
+          @char.skills.count.must_equal skill_count
+        else
+          @char.skills.count.must_equal skill_count + 1
+        end
+      end
+    end
+  end
+end
